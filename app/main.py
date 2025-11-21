a112ec204cced6913aab41bec19bfaab9dd89e88
vinay-dev95 <nvinay1702@gmail.com>
2025-11-21 20:57:05 +0530

    v1.0.0

---
+++ app/main.py
@@ -0,0 +1,5 @@
+def add(a, b):
+    return a + b
+
+def add(a,b,c):
+    return a + b + c
---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from app.main import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_add_three_args():
+    assert add(2, 3, 4) == 9
+    assert add(-1, 1, 1) == 1
+    assert add(0, 0, 0) == 0
